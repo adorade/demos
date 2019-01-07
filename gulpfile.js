f7f3676e6ba20cd78b55a4bd2acd03ae17a3e2f2
Marian Bolojan <cretu.adorade@gmail.com>
2019-01-07 15:44:46 +0200

    config(FILES): Update config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 /*!
  * Demos (v1.0.0): gulpfile.js
  * Copyright (c) 2019 Adorade (https://adorade.ro)
- * Licensed under MIT (https://github.com/adorade/demos/blob/master/LICENSE)
+ * Licensed under MIT
  * ============================================================================
  */
 'use strict';
